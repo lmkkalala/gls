12d99b1ce7f6ab1562d5af5780f6e0b46d0274c8
ElvisAns <ansimapersic@gmail.com>
2022-10-19 00:05:39 +0200

    fix typo

--- static/c_lire_article.php
+++ static/c_lire_article.php
@@ -83,7 +83,7 @@
       <div class="container">
 
         <div class="d-flex justify-content-between align-items-center">
-          <h2>Nouvelles Detaillés</h2>
+          <h2>Nouvelles Detaillées</h2>
           <ol>
             <li><a href="index.html">Accueil</a></li>
             <li><a href="blog.html">Nouvelles</a></li>
